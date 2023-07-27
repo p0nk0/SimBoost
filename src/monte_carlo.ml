c07f762787b0a2aacc4c61d5c2afb853fe1f191f
Sonica <sonicap2020@gmail.com>
2023-07-27 20:01:35 +0000

    removing dependency

--- src/monte_carlo.ml
+++ src/monte_carlo.ml
@@ -1,7 +1,17 @@
 open! Core
 open! Owl_base
 
-let main ~historical_dates ~historical_stock_prices =
-  ignore historical_dates;
-  ignore historical_stock_prices
-;;
+let main ~_historical_dates ~_historical_stock_prices = ()
+(* let main ~historical_dates ~historical_stock_prices = let let
+   _time_elapsed = Array.length historical_dates in let _g ignore
+   historical_stock_prices ;; *)
+
+(*Constraint -> dates must span at least two days and be two trading days for
+  data *)
+(* let total_growth_percentage ~stock_prices : float option = if Array.length
+   stock_prices < 2 then None else ( let last_element = Array.last
+   stock_prices in let first_element = Array.get stock_prices 0 in Some
+   (last_element /. first_element)) ;; *)
+
+(* let calc_annualized_growth_percentage = () let calc_scaled_std = () let
+   gen_rand_vals = () let accuracy = () *)
