e001268b5c45b98a872e4bee0eab6091697e4219
Sonica <sonicap2020@gmail.com>
2023-08-11 16:10:13 +0000

    modified help page

--- frontend/src/pages/Help.js
+++ frontend/src/pages/Help.js
@@ -27,6 +27,7 @@ export default function Help() {
                         Welcome to SimBoost!
                     </h1>
                     <h4> A Versatile Stock and Options Modeling Dashboard</h4>
+                    <h5> By: Sonica Prakash and Taylor Roberts </h5>
                     <h5>Mission Statement</h5>
                     
                     <p id = "paragraph"> We strive to help you make the most profit from your stock and options trading endeavors by boosting the power of your simulations. However, it is risky to deploy models for your trading strategies with your assets without first testing which models are optimal for your needs. We provide multiple, customizable models to predict stock and options prices for various companies in the S&P500. Feel free to experiment to see what sorts of parameters make the most optimal predictions for companies you might be interested in trading over. We make predictions over historical stock data so you can see how accurate our model’s predictions were in comparison to the true data of the time.  </p>
@@ -40,21 +41,15 @@ export default function Help() {
                     </ol>
                     <p id = "paragraph"> <b> Stock Price Prediction </b> </p>
                     <ul> <li class = "lists" id="paragraph"> <em> Monte Carlo </em> </li> </ul>
-                    <p id = "paragraph"> We will use the average of 10000 Monte Carlo simulation to predict the daily stock prices over a timeframe that you specify. </p>
-                    <p id = "paragraph"> Use the General Model Parameters to specify the start and ending dates over the entire time frame of stock data to be used in the model. </p>
-                    <p id = "paragraph"> Enter a prediction start date within the Start and End dates of the general model. </p>
-                    <p id = "paragraph"> Monte Carlo uses historical stock data from the General Start Date to Prediction Start Date to make daily stock predictions from the Prediction Start Date to General End Date. Predictions will also display on the graph along with a Mean Absolute Percentage Error!</p>
+                    <p id = "paragraph"> We will use the average of 10000 Monte Carlo simulation to predict the daily stock prices over a timeframe that you specify Use the General Model Parameters to specify the start and ending dates over the entire time frame of stock data to be used in the model. Enter a prediction start date within the Start and End dates of the general model. Monte Carlo uses historical stock data from the General Start Date to Prediction Start Date to make daily stock predictions from the Prediction Start Date to General End Date. Predictions will also display on the graph along with a Mean Absolute Percentage Error!</p>
                     <p id = "paragraph"> <b> Option Contract Pricing </b> </p>
                     <ul> <li class = "lists" id="paragraph"> <em> Black Scholes </em> </li> </ul>
                     <p id = "paragraph"> Black Scholes predicts prices for Call Option and Put Option contracts of 100 shares per contract. We assume a European type option in this case. </p>
-                    <p id = "paragraph"> Use the General Model Parameters to specify the start and ending dates over the entire time frame of stock data to be used in the model.  </p>
-                    <p id = "paragraph"> Input a Start Date for your contract. The model uses historical stock data from General Start Date to Contract Start Date to price an option that starts on the Contract Start Date and expires on the General End Date.  </p>
+                    <p id = "paragraph"> Use the General Model Parameters to specify the start and ending dates over the entire time frame of stock data to be used in the model. Input a Start Date for your contract. The model uses historical stock data from General Start Date to Contract Start Date to price an option that starts on the Contract Start Date and expires on the General End Date.  </p>
                     <p id = "paragraph"> Our model will provide the price for your Call/Put option and the PNL that you would have made if you traded the stock at the strike price of the contract and ending stock price of the company that you chose, taking into account the price of the option. </p>
                     <ul> <li class = "lists" id="paragraph"> <em> Binomial Pricer </em> </li> </ul>
-                    <p id = "paragraph"> Black Scholes predicts prices for Call Option and Put Option contracts of 100 shares per contract. We assume a European type option in this case. </p>
-                    <p id = "paragraph"> Use the General Model Parameters to specify the start and ending dates over the entire time frame of stock data to be used in the model.  </p>
-                    <p id = "paragraph"> Input a Start Date for your contract. The model uses historical stock data from General Start Date to Contract Start Date to price an option that starts on the Contract Start Date and expires on the General End Date.  </p>
-                    <p id = "paragraph"> Our model will provide the price for your Call/Put option and the PNL that you would have made if you traded the stock at the strike price of the contract and ending stock price of the company that you chose, taking into account the price of the option. </p>
+                    <p id = "paragraph"> The Binomial Pricer model also predicts prices for Call Option and Put Option contracts of 100 shares per contract, assuming a European type option. Use the General Model Parameters to specify the start and ending dates over the entire time frame of stock data to be used in the model.  Input a Start Date for your contract. The model uses historical stock data from General Start Date to Contract Start Date to price an option that starts on the Contract Start Date and expires on the General End Date. Also include the number of time steps used for model prediction. </p>
+                    <p id = "paragraph"> Like Black-Scholes, the model outputs the same results based on model parameters. </p>
                     
                     
                     </ThemeProvider>
